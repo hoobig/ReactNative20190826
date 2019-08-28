1ee1b3c474c6fd2a2e38ea79b1fcdf77ae077364
TriVH <console.log.big@gmail.com>
2019-08-28 10:56:57 +0700

    2808 2

--- src/components/index/IndexComponent.js
+++ src/components/index/IndexComponent.js
@@ -26,60 +26,65 @@ class IndexComponent extends Component {
         return (
             <View style={styles.container}>
             
-                <View style={styles.child}>
-                    <View style={styles.buttonContainer}>
-                        <TouchableOpacity style={styles.buttonCount} onPress={() => {this._onCountIncreasing()} }>
-                            <Text style={styles.text}>InCreasing</Text>
-                        </TouchableOpacity>
+            <View style={styles.child}>
+            <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.buttonCount} onPress={() => {this._onCountIncreasing()} }>
+            <Text style={styles.textButton}>InCreasing</Text>
+            </TouchableOpacity>
 
-                        <TouchableOpacity style={styles.buttonCount} onPress={() => {this._onCountDecreasing()} }>
-                            <Text style={styles.text}>DeCreasing</Text>
-                        </TouchableOpacity>
-                    </View>
-                    <Text>Count Number: { this.props.times }</Text>
-                </View>
+            <TouchableOpacity style={styles.buttonCount} onPress={() => {this._onCountDecreasing()} }>
+            <Text style={styles.textButton}>DeCreasing</Text>
+            </TouchableOpacity>
+            </View>
+            <Text>Count Number: { this.props.times }</Text>
+            </View>
 
-                <View style={styles.child}>
-                    <Text style={styles.text}>Hello dff</Text>
-                </View>
+            <View style={styles.child}>
+            <Text style={styles.text}>Hello dff</Text>
+            </View>
 
-                <View style={styles.child}>
-                    <Text style={styles.text}>Hello dff</Text>
-                </View>
+            <View style={styles.child}>
+            <Text style={styles.text}>Hello dff</Text>
+            </View>
 
             </View>
             );
-        }
     }
+}
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-        },
-        child: {
-            flex: 1,
-            backgroundColor: 'white',
-            borderBottomColor: 'black', 
-            borderBottomWidth: 3, 
-        },
-        buttonContainer: {
-            flex: 1,
-            flexDirection: 'row',
-            justifyContent: 'space-around'
-        },
-        buttonCount: {
-            width: 100,
-            height: 50,
-            borderColor: 'blue',
-            backgroundColor: 'green',
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        text: {
-            color: 'red'
-        }
-    });
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    },
+    child: {
+        flex: 1,
+        backgroundColor: 'white',
+        borderBottomColor: 'black', 
+        borderBottomWidth: 3, 
+    },
+    buttonContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around'
+    },
+    buttonCount: {
+        width: 100,
+        height: 50,
+        borderRadius: 5,
+        color: '#ffffff',
+        backgroundColor: '#6c757d',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+    },
+    textButton: {
+        color: '#ffffff'
+    },
+    text: {
+        color: 'red'
+    }
+});
 
-    export default IndexComponent;
+export default IndexComponent;
